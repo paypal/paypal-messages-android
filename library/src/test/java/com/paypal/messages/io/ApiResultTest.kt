222ebdc6289dd1ff95eb2d9b07752b3b9730acf1
Merlin (they/them) <124289716+merlinpaypal@users.noreply.github.com>
2023-10-15 00:29:53 -0400

    Add local storage test

--- library/src/test/java/com/paypal/messages/io/ApiResultTest.kt
+++ library/src/test/java/com/paypal/messages/io/ApiResultTest.kt
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ApiResultTest {
-
 	@Test
 	fun testSuccessToJson() {
 		val success = ApiResult.Success(
--- library/src/test/java/com/paypal/messages/totest/LocalStorageTest.kt
+++
@@ -1,3 +0,0 @@
-package com.paypal.messages.totest
-
-class LocalStorageTest
