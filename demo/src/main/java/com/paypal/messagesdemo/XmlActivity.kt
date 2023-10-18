9f19e98164a329957f84199f7c20268efa86b4c6
grablack <grablack@paypal.com>
2023-10-18 10:47:53 -0400

    Toast

--- demo/src/main/java/com/paypal/messagesdemo/XmlActivity.kt
+++ demo/src/main/java/com/paypal/messagesdemo/XmlActivity.kt
@@ -3,7 +3,7 @@ package com.paypal.messagesdemo
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.graphics.Color
 import com.google.android.material.switchmaterial.SwitchMaterial
