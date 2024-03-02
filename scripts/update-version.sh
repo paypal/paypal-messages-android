1eaa24d22ee1e52d7e2117a0be5c577916c9c2fc
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2024-03-02 12:52:00 -0500

    Fix script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -5,4 +5,4 @@ VERSION_MATCH='"[0-9a-zA-Z.-]+"'
 
 echo "Updating version to $VERSION"
 
-sed -i "s/sdkVersionName(.*): $VERSION_MATCH/sdkVersionName\1: $VERSION/" ./build.gradle
+sed -i -E "s/sdkVersionName(.*): $VERSION_MATCH/sdkVersionName\1: $VERSION/" ./build.gradle
