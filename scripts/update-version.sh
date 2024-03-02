f4f3c1ab93f86a952ec49c1b688c52c68e6e36ca
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2024-03-02 12:11:32 -0500

    Fix script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -4,4 +4,4 @@ VERSION=$1
 
 echo "Updating version to $VERSION"
 
-sed -i '' -E "s/sdkVersionName(.*): \"[0-9a-zA-Z.-]+\"/sdkVersionName\1: \"$VERSION\"/" ./build.gradle
+sed -i '' -E "s/sdkVersionName(.*): \"[0-9a-zA-Z.-]+\"/sdkVersionName\1: \"$VERSION\"/" ../build.gradle
