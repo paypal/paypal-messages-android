12687eaa47bd465dc3eb5b046924a39d156c758e
grablack <grablack@paypal.com>
2024-08-28 12:37:55 -0400

    test jetpack modal

--- demo/src/androidTest/java/com/paypal/messagesdemo/JetPackTest.kt
+++ demo/src/androidTest/java/com/paypal/messagesdemo/JetPackTest.kt
@@ -1,11 +1,14 @@
 package com.paypal.messagesdemo
 
+import android.content.Intent
 import android.view.Gravity
 import androidx.core.content.ContextCompat
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -20,8 +23,13 @@ import com.paypal.messagesdemo.R as Demo
 public class JetPackTest {
 	var expectedColor: Int? = null
 
-	@get:Rule
-	val activityScenarioRule = ActivityScenarioRule(JetpackActivity::class.java)
+	@Rule
+	@JvmField
+	val activityScenarioRule = ActivityScenarioRule<JetpackActivity>(
+		Intent(ApplicationProvider.getApplicationContext(), JetpackActivity::class.java).apply {
+			putExtra("TEST_ENV", "LIVE")
+		},
+	)
 
 	fun submit() {
 		onView(withId(Demo.id.submit)).perform(scrollTo())
@@ -30,9 +38,9 @@ public class JetPackTest {
 	}
 
 	@Test
-	fun testGenericBuyNowPayLaterMessage() {
+	fun testGenericMessage() {
 		// Perform a delay
-		waitForApp(500)
+		waitForApp(1000)
 
 		// Check if SecondActivity is displayed by verifying a TextView in SecondActivity
 		checkMessage("%paypal_logo% Buy now, pay later. Learn more")
@@ -46,5 +54,21 @@ public class JetPackTest {
 		// Use the custom matcher to check the text color of the TextView
 		onView(withId(R.id.content))
 			.check(matches(ColorMatcher.withTextColor(expectedColor!!)))
+	}
+
+	@Test
+	fun testGenericMessageAndModal() {
+		waitForApp(2000)
+
+		// Check if SecondActivity is displayed by verifying a TextView in SecondActivity
+		checkMessage("%paypal_logo% Buy now, pay later. Learn more")
+		clickMessage()
+
+		onView(withId(R.id.ModalWebView)).check(
+			matches(ViewMatchers.isDisplayed()),
+		)
+
+		modalContent("Pay Later options")
+		waitForApp(2000)
 	}
 }
